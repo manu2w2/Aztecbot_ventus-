a04e4f2dd203a795ffd290a435b354c7cf6125ac
manu2w2 <victor2rescod@gmail.com>
2026-03-04 00:30:12 -0600

    Add pruebas1 OpMode, update flywheel subsystem, and downgrade NextFTC control version

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsistemas/Shooter.kt
@@ -0,0 +1,80 @@
+package org.firstinspires.ftc.teamcode.subsistemas
+
+import com.bylazar.configurables.annotations.Configurable
+import com.qualcomm.robotcore.hardware.DcMotor
+import dev.nextftc.control.KineticState
+import dev.nextftc.control.builder.controlSystem
+import dev.nextftc.control.feedback.PIDCoefficients
+import dev.nextftc.control.feedforward.BasicFeedforwardParameters
+import dev.nextftc.core.commands.delays.WaitUntil
+import dev.nextftc.core.commands.groups.ParallelGroup
+import dev.nextftc.core.commands.groups.ParallelRaceGroup
+import dev.nextftc.core.subsystems.Subsystem
+import dev.nextftc.hardware.controllable.RunToVelocity
+import dev.nextftc.hardware.impl.MotorEx
+
+@Configurable
+object Flywheel : Subsystem {
+    private val motor = MotorEx("flywheelmotor1")
+    private val motor1 = MotorEx("flywheelmotor2").reversed()
+    // aca los declaro
+
+
+    var controlled = true
+    const val TICKS_REV = 37.333
+    // declaro varibles
+
+
+    @JvmField var basicFFCoefficients = BasicFeedforwardParameters(0.0,0.0,0.0)
+    @JvmField var velPIDCoefficients = PIDCoefficients(0.0,0.0,0.0)
+    // declaro la variables que sirve para mi pid que puedo editar desde panels
+
+
+
+
+        override fun initialize() {
+            motor.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
+            motor1.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
+
+            controller.goal = KineticState()
+    }
+    // aqui configuro mis motores
+
+
+    @JvmField
+        val controller = controlSystem {
+            velPid(velPIDCoefficients)
+            basicFF(basicFFCoefficients)
+        } // aca configuro mi controlador
+
+        val shootervelocity
+            get() = (motor.velocity + motor1.velocity)/2
+        var VueltasD = 0
+        val UmbralVueltas = 30
+        val RevisarVueltasHastaUmbral = WaitUntil {
+            if (VueltasD >= UmbralVueltas) {
+                true
+            } else { if(shootervelocity > (controller.goal.velocity - 10) && shootervelocity < (controller.goal.velocity + 40)){
+                VueltasD++
+                } else {
+                VueltasD = 0
+             }
+                false
+            }
+         }
+
+        val inicio = ParallelGroup(
+            RunToVelocity(
+                controller,
+                (2000.0/60.0) * TICKS_REV,
+                KineticState()
+            )
+        )
+
+        override fun periodic() {
+
+    }
+
+
+}
+
